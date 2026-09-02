87b733b33ed7d36a0decc3b7589b9ac9adaf0963
dandax123 <dandaoluks@gmail.com>
2026-09-02 10:55:49 +0200

    style: add asaerp style

--- erpnext/setup/doctype/company/company.py
+++ erpnext/setup/doctype/company/company.py
@@ -897,7 +897,7 @@ def update_company_current_month_sales(company):
 		)
 	).run(pluck=True)[0] or 0
 
-	# Fieldname in standard ERPNext is `total_monthly_sales`
+	# Fieldname in standard ASAERP is `total_monthly_sales`
 	frappe.db.set_value(
 		"Company",
 		company,
